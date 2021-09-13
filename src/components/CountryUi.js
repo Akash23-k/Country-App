7f6a3d949ec8809d4fa61cc56afec32c11f3936d
Khanapurkar <akash.khanapurkar@capgemini.com>
2021-09-13 11:09:33 +0530

    Country-Dropdown

--- src/components/CountryUi.js
+++ src/components/CountryUi.js
@@ -8,30 +8,43 @@ const CountryUi = (props) => {
     props.countryList();
   }, []);
 
-  const [country, setCountry] = useState([]);
+  const [country, setCountry] = useState();
 
   const selectHandleChange = (e) => {
     setCountry(e.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // props.countries.map((country) => {
+  //   //   var name;
+  //   //   if (country.name === name) {
+  //   //     setCountry(country);
+  //   //   }
+  //   //   return country;
+  //   // });
+  // };
+
   return (
     <div>
       <h4>Select Country from below dropdown and get the details</h4>
-      <form className="dropdown-form" onSubmit={handleSubmit}>
+      <form className="dropdown-form">
         <label className="form-labels">Select Country:</label>
         <select className="select-opt" onChange={selectHandleChange}>
           {props.countries.map((country) => {
-            return <option value={country}>{country.name}</option>;
+            return <option value={country.name}>{country.name}</option>;
           })}
         </select>
-        <button type="submit" className="submit-btn" value="Submit">
+        {/* <button
+          type="submit"
+          className="submit-btn"
+          onSubmit={handleSubmit}
+          value="Submit"
+        >
           Search
-        </button>
+        </button> */}
       </form>
-      {country.id}
+      <div className="form-labels">Selected Country : {country}</div>
     </div>
   );
 };
